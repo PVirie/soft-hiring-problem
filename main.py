86913ba403bdc9b53cc70ed4843db5467c08f5e8
Patrick Virie <p.virie@gmail.com>
2024-04-13 20:17:07 +0700

    added bearden strategy

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from math import comb
 
 def random_score(N, normal=False):
     if normal:
-        data = np.random.normal(500, 10, N)
-        # make sure all scores are positive
-        min_data = np.min(data)
-        data = (data - min_data) / (np.max(data) - min_data)
+        data = np.random.normal(0.5, 0.1666667, N)
+        # make sure all scores are positive and no more than 1
+        min_data = min(np.min(data), 0)
+        max_data = max(np.max(data), 1.0)
+        data = (data - min_data) / (max_data - min_data)
         return data
 
     return np.random.randint(0, 1000, N)/1000
@@ -74,19 +75,22 @@ def compute_expected_scores(scores, black_swan=True, oracle_bs=False):
 
     
 
-def run_experiment(M=1000, N=200):
+def run_experiment(M, N, normal):
 
     total_score_secretary = 0
     total_score_secretary_desperate = 0
+    total_score_secretary_bearden = 0
     total_score_rank_black_swan = 0
     total_score_soft_hiring_black_swan = 0
     total_score_rank_withhold = 0
     total_score_soft_hiring_withhold = 0
+    total_score_rank_wh_bearden = 0
+    total_score_soft_hiring_wh_bearden = 0
     total_score_rank_oracle_bs = 0
     total_score_soft_hiring_oracle_bs = 0
 
     for i in range(M):
-        scores = random_score(N, normal=False)
+        scores = random_score(N, normal=normal)
 
         p = execute_hiring(scores, N/math.e)
         if p >= 0:
@@ -95,10 +99,18 @@ def run_experiment(M=1000, N=200):
         else:
             total_score_secretary_desperate += scores[-1]
 
+        p = execute_hiring(scores, math.sqrt(N))
+        if p >= 0:
+            total_score_secretary_bearden += scores[p]
+        else:
+            total_score_secretary_bearden += scores[-1]
+
         rank_black_swan_picked = False
         soft_hiring_black_swan_picked = False
         rank_withhold_picked = False
         soft_hiring_withhold_picked = False
+        rank_wh_bearden_picked = False
+        soft_hiring_wh_bearden_picked = False
         rank_oracle_bs_picked = False
         soft_hiring_oracle_bs_picked = False
         
@@ -123,25 +135,38 @@ def run_experiment(M=1000, N=200):
 
         for j, (e, c) in enumerate(compute_expected_scores(scores, black_swan=False)):
             # no black swan, need trial period
-            if j <= N/math.e:
-                continue
 
-            if not soft_hiring_withhold_picked and e > 0.5:
-                total_score_soft_hiring_withhold += scores[j]
-                soft_hiring_withhold_picked = True
-
-            if not rank_withhold_picked and c > 0.5:
+            if j > N/math.e and not rank_withhold_picked and c > 0.5:
                 total_score_rank_withhold += scores[j]
                 rank_withhold_picked = True
 
-            if rank_withhold_picked and soft_hiring_withhold_picked:
+            if j > N/math.e and not soft_hiring_withhold_picked and e > 0.5:
+                total_score_soft_hiring_withhold += scores[j]
+                soft_hiring_withhold_picked = True
+
+            if j > math.sqrt(N) and not rank_wh_bearden_picked and e > 0.5:
+                total_score_rank_wh_bearden += scores[j]
+                rank_wh_bearden_picked = True
+
+            if j > math.sqrt(N) and not soft_hiring_wh_bearden_picked and c > 0.5:
+                total_score_soft_hiring_wh_bearden += scores[j]
+                soft_hiring_wh_bearden_picked = True
+
+            if rank_withhold_picked and soft_hiring_withhold_picked and rank_wh_bearden_picked and soft_hiring_wh_bearden_picked:
                 break
+
+        if not rank_withhold_picked:
+            total_score_rank_withhold += scores[-1]
 
         if not soft_hiring_withhold_picked:
             total_score_soft_hiring_withhold += scores[-1]
 
-        if not rank_withhold_picked:
-            total_score_rank_withhold += scores[-1]
+        if not rank_wh_bearden_picked:
+            total_score_rank_wh_bearden += scores[-1]
+
+        if not soft_hiring_wh_bearden_picked:
+            total_score_soft_hiring_wh_bearden += scores[-1]
+
 
         for j, (e, c) in enumerate(compute_expected_scores(scores, black_swan=True, oracle_bs=True)):
             if not soft_hiring_oracle_bs_picked and e > 0.5:
@@ -166,17 +191,25 @@ def run_experiment(M=1000, N=200):
     print()
     print("Secretary score", total_score_secretary/M)
     print("Secretary desperate score", total_score_secretary_desperate/M)
+    print("Secretary Bearden score", total_score_secretary_bearden/M)
     print("Rank score with black swan", total_score_rank_black_swan/M)
     print("Soft hiring score with black swan", total_score_soft_hiring_black_swan/M)
-    print("Rank score", total_score_rank_withhold/M)
+    print("Rank score with hold 1/e", total_score_rank_withhold/M)
     print("Soft hiring score with hold 1/e", total_score_soft_hiring_withhold/M)
+    print("Rank score with hold sqrt(N)", total_score_rank_wh_bearden/M)
+    print("Soft hiring score with hold sqrt(N)", total_score_soft_hiring_wh_bearden/M)
     print("Rank score oracle bs", total_score_rank_oracle_bs/M)
     print("Soft hiring score oracle bs", total_score_soft_hiring_oracle_bs/M)
 
 
 if __name__ == '__main__':
     
-    run_experiment()
+    print("Uniform distribution")
+    run_experiment(M=10000, N=200, normal=False)
+    print("=====================================")
+    print("Normal distribution")
+    run_experiment(M=10000, N=200, normal=True)
+    print("=====================================")
     
     N = 100
     X = np.arange(0, N)
